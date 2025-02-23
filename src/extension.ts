254ff68e71e12eaa2a94a11b82e283b8ed95ec30
Tomáš Král <kralutomas@gmail.com>
2025-02-23 17:22:02 +0100

    sort and find latest migration

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { commands, window, ExtensionContext, workspace } from "vscode";
+
+const frameworks = [{ id: "rails", name: "Ruby on Rails", folder: "db/migrate/*.rb" }];
+
+function showLatest() {
+	let found: boolean = false;
+
+	frameworks.forEach((framework) => {
+		workspace.findFiles(framework.folder).then((files) => {
+			if (files.length > 0) {
+				const paths = sortFilesByFramework(
+					framework.id,
+					files.map((file) => file.fsPath)
+				);
+
+				const latest = paths[paths.length - 1];
+				window.showInformationMessage(`Latest migration: ${latest}`);
+			}
+		});
+	});
+
+	window.showInformationMessage("No migrations found or framework not supported.");
+}
+
+function sortFilesByFramework(frameworkId: string, paths: string[]): string[] {
+	return paths.sort();
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
+export function activate(context: ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-show-migration" is now active!');
@@ -13,10 +38,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-show-migration.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ShowMigration!');
+	const disposable = commands.registerCommand("vscode-show-migration.showLatest", () => {
+		showLatest();
 	});
 
 	context.subscriptions.push(disposable);
